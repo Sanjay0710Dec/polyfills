215a4903edc528c48d8a92404d87b508dc61d6b5
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-07-07 08:03:13 +0530

    added error message

--- call.js
+++ call.js
@@ -1,6 +1,6 @@
 const person01 = {
     username:'something',
-    printUser:function(){
+    printUser:function(a,b){
         console.log(this)
         console.log(this.username);
     }
@@ -9,21 +9,26 @@ const person02 = {
     username:"nothing"
 }
 
+
 const person03 = {
     username:'something09',
     hobbies:"nothing"
 }
 
-//  person01.printUser.call(person02);
+ person01.printUser.call(person02);
 
 // If you understand prototype inheritance implementing call ,apply bind 
 
 Function.prototype.mycall = function(thisArg,...argsArray){
     // const func = this;
     // console.log(func === person01.printUser)
-    if(typeof(this) !== 'function'){
-        throw new Error()
+
+    if(typeof(thisArg) !== 'object'){
+        throw new Error('mycall expects object as first argument but got ',typeof(thisArg));
     }
+    // console.log(arguments.length)
+    // console.log(this.length)
+   
 
     Object.prototype.callBack = this;
     // console.log(person01.__proto__)
@@ -39,9 +44,11 @@ Function.prototype.mycall = function(thisArg,...argsArray){
 }
 
 
-person01.printUser.mycall(person02);
+person01.printUser.mycall(person02,100,100);
 
-person01.printUser.mycall(person03)
+person01.printUser.mycall(person03,100,100)
 
-console.log(person01.__proto__) // verify 29th line and this line 
+console.log(person01.__proto__) // verify 34th line and this line 
+
+
 
