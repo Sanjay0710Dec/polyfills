af910da385fcd2693103a76fcea874c3a9665a6e
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-07-09 09:04:35 +0530

    custom bind

---
+++ bind.js
@@ -0,0 +1,34 @@
+const person01 = {
+    username:'something',
+    printUser:function(a,b){
+        console.log(a,b);
+        console.log(this)
+        console.log(this.username);
+    }
+}
+
+const person02 = {
+    username:"nothing"
+}
+
+// bind under the hood uses call method.
+Function.prototype.myBind = function(thisArg,...argsArray){
+       const func = this;
+    if(typeof(thisArg) !== 'object'){
+        throw new Error('myBind expects object as first argument but got ',typeof(thisArg));
+    }
+
+    return function(...args){
+         func.call(thisArg,...argsArray,...args);
+    }
+}
+
+const something = person01.printUser.bind(person02);
+
+something(100,200);
+
+const something02 = person01.printUser.myBind(person02);
+something(200,300);
+
+// person01.myBind(person02);
+
