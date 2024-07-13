9e72cd721c949ddb2e6626f3c761112b21958781
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-07-13 09:44:36 +0530

    removed typeChecking of second parameter(thisArg)

--- map.js
+++ map.js
@@ -1,15 +1,12 @@
 const arr = [1,2,3,4,5,6];
 
 
-Array.prototype.myMap = function(callback,thisArg = {}){
+Array.prototype.myMap = function(callback,thisArg){
     const currentArr = this;
     const modifiedArr = [];
     //  console.log(callback.length);  // to find the no of arguments that call back function takes.
     if(typeof(callback) !== 'function'){
         throw new Error("myMap expects a callback function but got "+typeof(callback));
-    }
-    if(typeof(thisArg) !== 'object'){
-        throw new Error("myMap expects a thisArg to be Object but got "+typeof(thisArg));
     }
 
     for(let i =0; i < currentArr.length; i++){
