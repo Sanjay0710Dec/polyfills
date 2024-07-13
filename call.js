0b80da9597921e701e725a47a31db85e839caac5
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-07-13 09:36:54 +0530

    correctly implemented custom call method

--- call.js
+++ call.js
@@ -15,27 +15,30 @@ const person03 = {
     hobbies:"nothing"
 }
 
- person01.printUser.call(person02);
 
-// If you understand prototype inheritance implementing call ,apply bind 
+
+// If you understand prototype inheritance implementing call ,apply bind  will be very easy.
 
 Function.prototype.mycall = function(thisArg,...argsArray){
     // const func = this;
     // console.log(func === person01.printUser)
 
-    if(typeof(thisArg) !== 'object'){
-        throw new Error('mycall expects object as first argument but got ',typeof(thisArg));
-    }
+    // if(typeof(thisArg) !== 'object'){
+    //     throw new Error('mycall expects object as first argument but got ',typeof(thisArg));
+    // }
     // console.log(arguments.length)
     // console.log(this.length)
    
+ if(thisArg === undefined || thisArg === null){
+        this(...argsArray);
 
-    Object.prototype.callBack = this;
-    // console.log(person01.__proto__)
-
-     thisArg.callBack(...argsArray);
-     
-     delete Object.prototype.callBack
+   }
+   else{
+      Object.prototype.callBack = this;
+    //   console.log(person01.__proto__) 
+      thisArg.callBack(...argsArray);
+      delete Object.prototype.callBack
+   }
 
 
 //    thisArg.myfunc = func;
@@ -43,12 +46,33 @@ Function.prototype.mycall = function(thisArg,...argsArray){
       
 }
 
+// TestCases For mycall
 
-person01.printUser.mycall(person02,100,100);
+person01.printUser.mycall(undefined,99);
+person01.printUser.mycall(null,99)
+person01.printUser.mycall(100,99);
+person01.printUser.mycall(true,99);
+person01.printUser.mycall("nothing",99);
+person01.printUser.mycall(person02,99);
+person01.printUser.mycall(function(){
+    console.log("something")
+},99)
 
-person01.printUser.mycall(person03,100,100)
+// TestCases For in-built call
+person01.printUser.call(undefined,99);
+person01.printUser.call(null,99)
+person01.printUser.call(100,99);
+person01.printUser.call(true,99);
+person01.printUser.call("nothing",99);
+person01.printUser.call(person02,99);
 
-console.log(person01.__proto__) // verify 34th line and this line 
+person01.printUser.call(function(){
+    console.log("something")
+},99);
+
+
+
+console.log(person01.__proto__) // verify 38th line and this line 
 
 
 
